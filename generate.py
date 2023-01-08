810cab18f313510aad04f857a88376d9e86b4a1c
Finley Li <li.f@wustl.edu>
2023-01-08 10:14:52 -0600

    checkpoint

--- generate.py
+++ generate.py
@@ -3,12 +3,22 @@ import pyrosim.pyrosim as pyrosim
 # tell pyrosim the name of the file where information about the world you're about to create should be stored.
 pyrosim.Start_SDF("boxes.sdf")
 
-length,width,height=1,1,1
+x, y, = 0, 0
 
-x1,y1,z1=0,0,0.5
-x2,y2,z2=1,0,1.5
 # store a box with initial position and dimension
-pyrosim.Send_Cube(name="Box", pos=[x1,y1,z1], size=[length, width, height])
-pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2], size=[length, width, height])
-#close file
+
+for row in range(5):
+    for col in range(5):
+        length, width, height = 1, 1, 1
+        z = 0.5
+        for i in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[row, col, z], size=[length, width, height])
+            z += height
+            length *= 0.9
+            width *= 0.9
+            height *= 0.9
+
+# pyrosim.Send_Cube(name="Box", pos=[x1,y1,z1], size=[length, width, height])
+# pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2], size=[length, width, height])
+# close file
 pyrosim.End()
