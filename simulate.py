b07187d39103523a74ab59809cf082122eb8b87c
Finley Li <li.f@wustl.edu>
2023-01-07 22:24:56 -0600

    update

--- simulate.py
+++ simulate.py
@@ -2,6 +2,9 @@ import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
+
+#read in the world described in box.sdf
+p.loadSDF("box.sdf")
 for i in range(1000):
     print(i)
     time.sleep(1/60)
